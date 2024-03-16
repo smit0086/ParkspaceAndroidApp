9762f3550f13b08d6f8aabdcecc3c9986653c1d4
Dheemanth Rajendra Prasad Kumawat <dh243394@dal.ca>
2024-03-16 18:30:44 -0300

    updated routes

--- app/src/main/java/com/parkspace/finder/navigation/Routes.kt
+++ app/src/main/java/com/parkspace/finder/navigation/Routes.kt
@@ -3,4 +3,4 @@ package com.parkspace.finder.navigation
 const val ROUTE_LOGIN = "login"
 const val ROUTE_SIGNUP = "signup"
 const val ROUTE_HOME = "home"
-const val ROUTE_DETAILS = "parkingDetails"
+const val ROUTE_DETAILS = "details"
